2f65675596c99e37a75c529065b2fe504bb48890
Sebastien Clemenceau <sebastien.clemenceau@offratel.net>
2018-10-30 12:04:40 +1100

    trying

--- root/usr/local/bin/entrypoint.sh
+++ root/usr/local/bin/entrypoint.sh
@@ -3,11 +3,13 @@ set -e
 
 if [ $JACKETT_PRE_BUILD -eq 1 ]; then
   url="https://api.github.com/repos/Jackett/Jackett/releases"
+  vers="PRE-RELEASE"
 else
   url="https://api.github.com/repos/Jackett/Jackett/releases/latest"
+  vers="RELEASE"
 fi
 
-echo "**** install jackett ****"
+echo "**** install jackett $vers ****"
 mkdir -p /app/Jackett
 jack_tag=$(curl -sX GET "$url" | awk '/tag_name/{print $4;exit}' FS='[""]')
 curl -o /tmp/jacket.tar.gz -L https://github.com/Jackett/Jackett/releases/download/$jack_tag/Jackett.Binaries.Mono.tar.gz
